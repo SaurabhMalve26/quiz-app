915b71e02f8b9ab2c38ec9591ab0a01b87aba058
Saurabh Malve <malve.saurabh45@gmail.com>
2024-06-11 12:55:23 +0530

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data=[
+    {
+        question : "Which of the following is used in React.js to increase performance?",
+        option1: "Virtual DOM",
+        option2: "Orignal DOM",
+        option3: "Both A and B",
+        option4: "None Of The Above",
+        ans:1
+    },
+    {
+        question : "What is ReactJS ?",
+        option1: "Server Side Framework",
+        option2: "User Interface Framework",
+        option3: "Both A and B",
+        option4: "None Of The Above",
+        ans:2
+    },
+    {
+        question : "Identify the one which is used to pass data to components from outside ?",
+        option1: "Render with arguments",
+        option2: "setState",
+        option3: "PropType",
+        option4: "props",
+        ans:4
+    },
+    {
+        question : "Which of the following port is the default where webpack-dev-server runs?",
+        option1: "3000",
+        option2: "3306",
+        option3: "3303",
+        option4: "8080",
+        ans:4
+    },
+    {
+        question : "Which of the following method is used to access the state of a component from inside of a member function?",
+        option1: "this.prototype.stateValue",
+        option2: "this.getState()",
+        option3: "this.values",
+        option4: "this.state",
+        ans:3
+    }
+];
